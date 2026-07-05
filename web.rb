978dc64d836db7525da221691adf0e350410dd25
Vyctor Hugo <vyctor2017hugo@gmail.com>
2026-07-05 16:17:56 -0300

    Adiciona validações de segurança no formulário de vendas

--- web.rb
+++ web.rb
@@ -22,8 +22,16 @@ get '/' do
 end
 
 post '/adicionar' do
-  File.open('vendas.csv', 'a') do |f|
-    f.puts "#{params[:data]},#{params[:produto]},#{params[:vendedor]},#{params[:valor]}"
+  if params[:data].empty? || params[:produto].empty? || params[:vendedor].empty? || params[:valor].empty?
+    return "Erro: Todos os campos são obrigatórios! <a href='/'>Voltar</a>"
   end
+  valor = params[:valor].to_f
+  if valor <= 0
+    return "Erro: O valor deve ser um número maior que zero! <a href='/'>Voltar</a>"
+  end
+  File.open('vendas.csv', 'a') do |f|
+    f.puts "#{params[:data]},#{params[:produto]},#{params[:vendedor]},#{valor}"
+  end
+  
   redirect '/'
 end
